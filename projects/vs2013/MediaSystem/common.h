6f944a9b464c95d9a9fa72ac33ee6a73dd491f8f
wangdm <wangdm1989@gmail.com>
2015-01-14 15:00:52 +0800

    add vs2013 project file

---
+++ projects/vs2013/MediaSystem/common.h
@@ -0,0 +1,7 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include "sysconfig.h"
+
+
+#endif // !_COMMON_H_
---
+++ projects/vs2013/MediaSystem/sysconfig.h
@@ -0,0 +1,6 @@
+#ifndef _SYSCONFIG_H_
+#define _SYSCONFIG_H_
+
+
+
+#endif // !_SYSCONFIG_H_
